f0c28fc4e86cac74b0a84beb40265b1d91dd7a15
jtenner <tenner.joshua@gmail.com>
2026-02-25 08:12:39 -0500

    Cleanups

--- scripts/concat.ts
+++
@@ -1,8 +0,0 @@
-for await (const file of new Bun.Glob("src/**/*.mbt").scan()) {
-  const text = await Bun.file(file).text()
-  console.log(`// ${file}`);
-  console.log(text);
-  console.log("");
-}
-
-export {}
