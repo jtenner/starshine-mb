ab6b1ed897f1915b2f6ad666e8619e151bf33046
jtenner <tenner.joshua@gmail.com>
2026-03-09 07:19:15 -0400

    Reformat the command to make it more readable

--- scripts/self-optimize.sh
+++ scripts/self-optimize.sh
@@ -1,3 +1,9 @@
 moon clean
 moon build --target wasm
-STARSHINE_TRACE_OPTIMIZE=1 STARSHINE_TRACE_OPTIMIZATION_VERBOSE=1 OPTIMIZE_DUMP_FAILED_MODULE_STATE=1 moon run src/cmd --target native --release -- --optimize -O4z --out tests/node/dist/starshine-self-optimized-wasi.wasm ./_build/wasm/debug/build/cmd/cmd.wasm > output.log 2>&1
+STARSHINE_TRACE_OPTIMIZE=1 STARSHINE_TRACE_OPTIMIZATION_VERBOSE=1 OPTIMIZE_DUMP_FAILED_MODULE_STATE=1 \
+  moon run src/cmd \
+  --target native \
+  --release \
+  -- \
+  --optimize -O4z \
+  --out tests/node/dist/starshine-self-optimized-wasi.wasm ./_build/wasm/debug/build/cmd/cmd.wasm > output.log 2>&1
