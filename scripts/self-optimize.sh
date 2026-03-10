cc60331f390e2a808ce9272a5b1e6a9d7bbfeff6
jtenner <tenner.joshua@gmail.com>
2026-03-10 13:11:19 -0400

    Simple change in how tracing is called in the self-optimize script

--- scripts/self-optimize.sh
+++ scripts/self-optimize.sh
@@ -1,9 +1,10 @@
 moon clean
 moon build --target wasm
-STARSHINE_TRACING=PASS OPTIMIZE_DUMP_FAILED_MODULE_STATE=1 \
+OPTIMIZE_DUMP_FAILED_MODULE_STATE=1 \
   moon run src/cmd \
   --target native \
   --release \
   -- \
+  --tracing helper \
   --optimize -O4z \
   --out tests/node/dist/starshine-self-optimized-wasi.wasm ./_build/wasm/debug/build/cmd/cmd.wasm > output.log 2>&1
